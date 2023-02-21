1100b5b6f9946a0e46085e943f2fe4b2f99403ef
indhu2 <indhumathik32@gmail.com>
2023-02-22 01:06:17 +0530

    status bar & lock file changes done

--- src/common/components/MainFrame/index.tsx
+++ src/common/components/MainFrame/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SafeAreaView, StatusBar, StatusBarStyle, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-// import {SafeAreaView} from 'react-native-safe-area-context';
 import { Colors, CommonStyles } from "../../res";
 import Header from "../Header";
 
@@ -21,23 +20,25 @@ export const MainFrame: React.FunctionComponent<MainFrameProps> = ({
   contentContainerStyle,
   scrollable,
   containerStyle,
-  barStyle = "light-content",
+  barStyle = "dark-content",
   backgroundColor,
   showStatusBar,
   header,
 }) => {
   return (
     <View style={{ flex: 1 }}>
-      <SafeAreaView
-        style={{ backgroundColor: backgroundColor ?? Colors.primary,  }}
-        edges={["top", "right", "left"]}
-      />
       {/* <StatusBar
         translucent
         hidden={!showStatusBar}
         backgroundColor={backgroundColor ?? Colors.primary}
         barStyle={barStyle}
-      /> */}
+      /> */} 
+      <StatusBar backgroundColor={backgroundColor ?? Colors.primary}/>
+      <SafeAreaView
+        style={{ backgroundColor: backgroundColor ?? Colors.primary,  }}
+        edges={["top", "right", "left"]}
+      />
+      
       {header && header}
       {scrollable ? (
         <KeyboardAwareScrollView
