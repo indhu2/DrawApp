f4fa60e3232b40799791ea90c94dec2bebf7ae7c
indhu2 <indhumathik32@gmail.com>
2023-02-22 23:09:46 +0530

    removed unused components & styles

--- src/common/res/Images.ts
+++
@@ -1,6 +0,0 @@
-const Images = {
-    //sample -- image: require('@root/assets/images/png/image_name.png'),
-
-}
-
-export default Images
--- src/common/res/index.ts
+++ src/common/res/index.ts
@@ -1,6 +1,4 @@
 import Colors from "./Colors";
 import Strings from "./Strings";
-import Images from './Images';
-import CommonStyles from "./CommonStyles";
 
-export {Colors, Strings, Images, CommonStyles}
+export {Colors, Strings}
